f72c278bd630479fff390297fd757ebe85bd192f
ceasius <Justin.Swanepoel@live.com>
2019-04-02 20:26:16 +0200

    Added Morgan logging

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-const genres = require("./routes/genres");
-const express = require("express");
-const swaggerUi = require("swagger-ui-express"),
-  swaggerDocument = require("./swagger.json");
+const express = require('express');
+const morgan = require('morgan');
+const genres = require('./routes/genres');
+const swaggerUi = require('swagger-ui-express'),
+  swaggerDocument = require('./swagger.json');
 
 const app = express();
+const env = app.get('env');
 
+if (env === 'development') {
+  app.use(morgan('tiny'));
+}
 app.use(express.json());
-app.use("/api/genres", genres);
+app.use('/api/genres', genres);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.get("/", (req, res) => {
-  res.redirect("/api-docs");
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
 });
 
 const port = process.env.PORT || 3000;
+console.log('Environment:', env);
 app.listen(port, () => console.log(`Listening on port ${port}...`));
