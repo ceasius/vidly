98162cf99a28dc413b90af7ea0f1fe9f6ca916f3
ceasius <Justin.Swanepoel@live.com>
2019-04-02 19:41:54 +0200

    Added Get Redirect

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const genres = require("./routes/genres");
 const express = require("express");
-const app = express();
 const swaggerUi = require("swagger-ui-express"),
   swaggerDocument = require("./swagger.json");
+
+const app = express();
 
 app.use(express.json());
 app.use("/api/genres", genres);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/", (req, res) => {
+  res.redirect("/api-docs");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
