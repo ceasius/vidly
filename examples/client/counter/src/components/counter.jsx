27c4ad734def1a17d628885ff1db623becc4c653
ceasius <Justin.Swanepoel@live.com>
2019-05-05 23:36:47 +0200

    Changed source of truth

--- examples/client/counter/src/components/counter.jsx
+++ examples/client/counter/src/components/counter.jsx
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    count: 0
-  };
-  constructor() {
-    super();
-  }
+  state = {};
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           +
         </button>
-      </React.Fragment>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+
   getBadgeClasses() {
     let badges = 'badge m-2 badge-';
-    return (badges += this.state.count === 0 ? 'warning' : 'primary');
+    return (badges += this.props.counter.value === 0 ? 'warning' : 'primary');
   }
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
   }
 }
 
