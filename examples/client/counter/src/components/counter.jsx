b30fae6053eb29c191800e4a38325143cde53964
ceasius <Justin.Swanepoel@live.com>
2019-06-11 20:16:12 +0200

    Update counter.jsx
    Added WillUpdate & WillUnmount functionality

--- examples/client/counter/src/components/counter.jsx
+++ examples/client/counter/src/components/counter.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps);
+    console.log('prevState', prevState);
+  }
+
+  componentWillUnmount() {
+    console.log('Counter - Unmount');
+  }
+
   state = {};
   render() {
     const { onIncrement, onDelete, counter } = this.props;
